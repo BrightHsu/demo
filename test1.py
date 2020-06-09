e0651cbd44154c6e4422a05c3578f9432c4917fd
HsuDavid <hsudavid@163.com>
2020-06-09 15:11:26 +0800

    add test1,2

---
+++ test1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    @Time    : 2020/6/9 15:10
+    @Author  : David Ben
+    @FileName: test1.py
+    @FileName: hsudavid@163.com     
+    @Software: PyCharm
+"""
+
+print('test1')
+print('test1')
